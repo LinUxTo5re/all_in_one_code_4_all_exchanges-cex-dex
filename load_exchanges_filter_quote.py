bedeb786e8e0a862b4f4ad07d14978e90288e5b4
LinUxTo5re <swapniltikhe222@gmail.com>
2023-05-10 00:37:56 +0530

    ccxt_2: Adding new changes. created new python file

--- load_exchanges_filter_quote.py
+++ load_exchanges_filter_quote.py
@@ -10,7 +10,6 @@ final output will be common markets from those exchanges.
 # Importing Required libraries
 import ccxt
 
-
 # declaring global variables
 exchanges_to_compare, exchanges_names = 0, []
 unique_quote_assets = set()
@@ -23,6 +22,8 @@ assets_names = assets_names_final = assets_copied = []
     exchanges number should be greater than 1 and less than 
     maximum length of ccxt.exchages length.
 '''
+
+
 class loadFilterMarkets:
 
     def __init__(self):
@@ -32,8 +33,13 @@ class loadFilterMarkets:
         while True:
             try:
                 global exchanges_to_compare, exchanges_names
+                exchanges_to_compare = 0, exchanges_names.clear()
+                # exchanges_to_compare = int(
+                #     input(f"How many exchanges do you want to compare? (Total: {len(ccxt.exchanges)} exchanges): "))
+
                 exchanges_to_compare = int(
-                    input(f"How many exchanges do you want to compare? (Total: {len(ccxt.exchanges)} exchanges): "))
+                    input(
+                        f"Please Select Only 2 Exchanges(project is in Initial Stage) (Total: {len(ccxt.exchanges)} exchanges): "))
 
                 if exchanges_to_compare < 2 or exchanges_to_compare > len(ccxt.exchanges):
                     raise ValueError(f"Number of exchanges should be between 2 and {len(ccxt.exchanges)}.")
@@ -49,13 +55,12 @@ class loadFilterMarkets:
                 self.create_variables_for_selected_exchanges_assets(
                     exchanges_names)  # calling function to create variables for selected exchanges
                 print(f"Selected exchanges are: {exchanges_names}\n")
+                return exchanges_names
                 break
-
 
     '''
     loading market data for selected exchanges
     '''
-
 
     def load_markets_for_selected_exchanges(self, exchange=None):
         if exchange is not None:
@@ -68,12 +73,10 @@ class loadFilterMarkets:
                     exchange = exchange.load_markets()
             return True
 
-
     '''
         loading quote assets for selected exchanges 
         and filtering unique assets from those exchanges
     '''
-
 
     def load_quote_assets_and_filter_common_assets(self):
         global unique_quote_assets, quote_assets_specific_2_exchange, exchanges_names
@@ -108,13 +111,11 @@ class loadFilterMarkets:
         quote_assets_specific_2_exchange['common quote assets'] = unique_quote_assets
         return unique_quote_assets
 
-
     '''
     load markets for selected quote assets based 
     on selected exchanges and store into 
     exchangeName_quoteAsset format
     '''
-
 
     def load_market_for_selected_quote_asset(self):
         global assets_names, assets_copied
@@ -122,8 +123,20 @@ class loadFilterMarkets:
             self.load_quote_assets_and_filter_common_assets()
 
         print(f'{list(unique_quote_assets)}')
+        # asset_to_load_market = input(
+        #     f"select at least one asset to proceed <space> (Total: {len(unique_quote_assets)} assets): ")
+
+        if len(unique_quote_assets) == 0:
+            print('No common asset... redirecting to select another pair of exchanges')
+            self.exchange_selection()
+
         asset_to_load_market = input(
-            f"select at least one asset to proceed <space> (Total: {len(unique_quote_assets)} assets): ")
+            f"select only one asset to proceed(project is in Initial stage) (Total: {len(unique_quote_assets)} assets): ")
+
+        if len(asset_to_load_market) == 0:
+            print('No common asset... redirecting to select another pair of exchanges')
+            self.exchange_selection()
+
         assets_names = asset_to_load_market.split(' ')
         assets_copied = assets_names.copy()
 
@@ -136,76 +149,110 @@ class loadFilterMarkets:
         assets_names = assets_names_final.copy()
         for asset_id, asset in enumerate(assets_copied):  # ex- asset = 'usdt', asset_id = 0
             for exchange_id, exchange in enumerate(exchanges_names):  # ex- exchange = 'bybit', exchange_id = 0
-                for asset_var_id, asset_var in enumerate(assets_names):  # ex- asset_var = 'bybit_usdt', asset_var_id = 0
+                for asset_var_id, asset_var in enumerate(
+                        assets_names):  # ex- asset_var = 'bybit_usdt', asset_var_id = 0
                     if asset_var.startswith(str(exchange)) and asset_var.endswith(str(asset)):
                         if (asset_var in globals()) and (exchange in globals()):
                             try:
                                 temp_store = self.load_markets_for_selected_exchanges(exchange)
                                 globals()[asset_var] = []
                                 for market in temp_store:
+                                    if ':' in market:
+                                        continue
                                     if market.endswith(str(asset).upper()):
                                         globals()[asset_var].append(market)
+                                print("{}-{} markets: {}".format(exchange, asset, len(globals()[asset_var])))
+
+
                             except Exception as e:
                                 print("Error: while loading market")
-
 
     '''
         this function will find common markets like common USDT markets
         from selected exchanges
     '''
 
+    def find_common_markets(self, iscalledagain = None):
+        global assets_names, assets_copied, variable_name, exchanges_names
 
-    def find_common_markets(self):
-        global assets_names, assets_copied, variable_name
+        if len(assets_copied) == 0 or iscalledagain:
+            unique_quote_assets.clear()
+            assets_copied.clear()
+            assets_names.clear()
+            exchanges_to_compare = 0
+            exchanges_names.clear()
+            unique_quote_assets.clear()
+            quote_assets_specific_2_exchange.clear()
+            variable_name = ''
+            assets_names_final.clear()
 
-        for market in assets_copied:  # ex- market = 'usdt'
-            filtered_markets_variables = list(filter(lambda k: k.endswith(str(market).lower()), globals().keys()))
-            self.create_variables_for_selected_exchanges_assets(
-                final_market=market)  # ex- final_market_usdt, final_market_btc etc
+            self.load_market_for_selected_quote_asset()
 
-            if set(filtered_markets_variables).issubset(set(globals().keys())):
-                common_markets = set()
-                for common_market in range(len(filtered_markets_variables)):
-                    common_market_1 = set(globals()[filtered_markets_variables[common_market]])
+        try:
+            for market in assets_copied:  # ex- market = 'usdt'
+                filtered_markets_variables = list(filter(lambda k: k.endswith(str(market).lower()), globals().keys()))
+                self.create_variables_for_selected_exchanges_assets(
+                    final_market=market)  # ex- final_market_usdt, final_market_btc etc
 
-                    if common_market != len(filtered_markets_variables) - 1:
-                        common_market_next = set(globals()[filtered_markets_variables[common_market + 1]])
+                if set(filtered_markets_variables).issubset(set(globals().keys())):
+                    common_markets = set()
+                    for common_market in range(len(filtered_markets_variables)):
+                        common_market_1 = set(globals()[filtered_markets_variables[common_market]])
 
-                    if common_market == 0:
-                        globals()[variable_name] = common_market_1.intersection(common_market_next)
-                    else:
-                        if common_market == len(filtered_markets_variables) - 1:
-                            break
+                        if common_market != len(filtered_markets_variables) - 1:
+                            common_market_next = set(globals()[filtered_markets_variables[common_market + 1]])
+
+                        if common_market == 0:
+                            globals()[variable_name] = common_market_1.intersection(common_market_next)
                         else:
-                            globals()[variable_name] = globals()[variable_name].intersection(common_market_next)
+                            if common_market == len(filtered_markets_variables) - 1:
+                                # final_common_market = globals()[variable_name]
+                                print("{}-{} common markets".format(len(globals()[variable_name]), variable_name))
+                                return globals()[variable_name]
+                            else:
+                                globals()[variable_name] = globals()[variable_name].intersection(common_market_next)
+                                print("{}-{} common markets".format(len(globals()[variable_name]), variable_name))
+                                return globals()[variable_name]
+        except Exception as e:
+            print("Error: check find_common_market() - " + str(e))
 
+        finally:
+            return globals()[variable_name], exchanges_names, assets_copied
 
     '''
     function will create variables for exchanges_names/unique_quote_assets/final markets
     so, we can use these variables later to assign values.
     also, all created varaibles can be found in globals().keys()
+    pass final_market as single parameter - don't pass with any another params
     '''
 
-
-    def create_variables_for_selected_exchanges_assets(self, exchanges_names=None, unique_quote_assets=None, final_market=None):
+    def create_variables_for_selected_exchanges_assets(self, exchanges_names = None, unique_quote_assets = None,
+                                                       final_market = None):
         global variable_name
+        try:
 
-        if final_market is not None:
-            variable_name = f"final_market_{final_market.lower()}"  # final_market_usdt
-            globals()[variable_name] = []
-        else:
-            assets_names_final.clear()
-            exchanges = {name: getattr(ccxt, name)() for name in exchanges_names}
+            if final_market is not None:
+                variable_name = f"final_market_{final_market.lower()}"  # final_market_usdt
+                globals()[variable_name] = []
+            else:
+                assets_names_final.clear()
 
-            # create variables for entered exchanges
-            for exchange_name, exchange_instance in exchanges.items():
-                globals()[exchange_name] = exchange_instance
-                
-                if unique_quote_assets is not None:  # code will run when creating variables for quote_assets
-                    for quote in unique_quote_assets:
-                        variable_name = f"{exchange_name}_{quote.lower()}"
-                        globals()[variable_name] = variable_name
-                        assets_names_final.append(variable_name)
+                if exchanges_names is None:
+                    return 0
+
+                exchanges = {name: getattr(ccxt, name)() for name in exchanges_names}
+
+                # create variables for entered exchanges
+                for exchange_name, exchange_instance in exchanges.items():
+                    globals()[exchange_name] = exchange_instance
+
+                    if unique_quote_assets is not None:  # code will run when creating variables for quote_assets
+                        for quote in unique_quote_assets:
+                            variable_name = f"{exchange_name}_{quote.lower()}"
+                            globals()[variable_name] = variable_name
+                            assets_names_final.append(variable_name)
+        except Exception as e:
+            print("Error: while loading market - " + str(e))
 
 
 if __name__ == '__main__':
